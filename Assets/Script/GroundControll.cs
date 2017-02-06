3d0abe85be4e01f791c28a9a4e9dd741599a1a83
Jooyoun Lee <jjooyoun@gmail.com>
2017-02-06 15:01:31 -0800

    Endless Running Prototype

---
+++ Assets/Script/GroundControll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundControll : MonoBehaviour {
+
+    //Material texture offset rate
+    float speed = .5f;
+
+    //Offset the material texture at a constant rate
+    void Update()
+    {
+        float offset = Time.time * speed;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, -offset);
+    }
+}
