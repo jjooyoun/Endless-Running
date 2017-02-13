1bbb12e1a2a30506d0bbc17fed37ecf22c87154a
matias <matiasfr@usc.edu>
2017-02-13 14:21:32 -0800

    added better movement switching logic

--- Assets/PlayerMovementController.cs
+++ Assets/PlayerMovementController.cs
@@ -8,9 +8,15 @@ public class PlayerMovementController : MonoBehaviour {
 	public Transform CenterLanePos;
 	public Transform RightLanePos;
 
+	Lane currentLane;
+
+
+	enum Lane {Left, Center, Right};
+
 
 	// Use this for initialization
 	void Start () {
+		currentLane = Lane.Center;
 		transform.position = CenterLanePos.position;
 
 	}
@@ -18,35 +24,62 @@ public class PlayerMovementController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate ( new Vector3(1,0,0) * ( 150.0f * Time.deltaTime ) );
-
+			
 		if(Input.GetKeyDown(KeyCode.LeftArrow)) {
-			SwitchLaneLeft();
-		}
-
-		if(Input.GetKeyDown(KeyCode.UpArrow)) {
-			SwitchLaneCenter();
+			MoveLeft();
 		}
 
 		if(Input.GetKeyDown(KeyCode.RightArrow)) {
-			SwitchLaneRight();
+			MoveRight();
 		}
+	}
+
+	void MoveLeft() {
+		if( currentLane == Lane.Left) {
+			//do nothing, already in left lane
+		}
+		if( currentLane == Lane.Center) {
+			//move to left lane
+			GoLaneLeft();
+		}
+		if(currentLane == Lane.Right) {
+			//move to center lane
+			GoLaneCenter();
+		}
+	}
+
+	void MoveRight() {
+		if(currentLane == Lane.Right) {
+			//do nothing, already in right lane
+		}
+		if( currentLane == Lane.Center) {
+			//move to Right lane
+			GoLaneRight();
+		}
+		if( currentLane == Lane.Left) {
+			//move to center lane
+			GoLaneCenter();
+		}
+	}
 
 
+	void GoLaneLeft() {
+		transform.position = LeftLanePos.position;
+		currentLane = Lane.Left;
+
+	}
+
+	void GoLaneCenter() {
+		transform.position = CenterLanePos.position;
+		currentLane = Lane.Center;
+			
+	}
+
+	void GoLaneRight() {
+		transform.position = RightLanePos.position;
+		currentLane = Lane.Right;
 			
 	}
 
 
-	void SwitchLaneLeft() {
-		transform.position = LeftLanePos.position;
-	}
-
-	void SwitchLaneCenter() {
-		transform.position = CenterLanePos.position;
-
-	}
-
-	void SwitchLaneRight() {
-		transform.position = RightLanePos.position;
-
-	}
 }
