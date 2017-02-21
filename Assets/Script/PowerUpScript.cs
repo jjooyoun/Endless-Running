47cea71ba65d92603eff063ada2beaec58f11377
Jooyoun Lee <jjooyoun@gmail.com>
2017-02-21 11:15:55 -0800

    Generate PowerUp & Obstacle

---
+++ Assets/Script/PowerUpScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpScript : MonoBehaviour {
+
+    public float objectSpeed = -0.5f;
+
+    void Update()
+    {
+        transform.Translate(0, 0, objectSpeed);
+    }
+}
--- Assets/Script/SpawnScript.cs
+++ Assets/Script/SpawnScript.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class SpawnScript : MonoBehaviour {
 
+    public GameObject powerup;
     public GameObject obstacle;
-	public Transform[] lanes;
+    public Transform[] lanes;
     float timeElapsed = 0;
     float spawnCycle = 0.5f;
+    bool spawnPowerup = true;
 
     void Update()
     {
         timeElapsed += Time.deltaTime;
         if (timeElapsed > spawnCycle)
         {
-            //            GameObject tmp = (GameObject)Instantiate(obstacle, new Vector3(0, 1, 42), Quaternion.identity);
-            GameObject tmp = (GameObject)GameObjectUtil.Instantiate(obstacle, new Vector3(0, 1, 42));
-            //Vector3 pos = obstacle.transform.position;
-            tmp.transform.position = lanes[Random.Range (0, lanes.Length)].position;
+            if (spawnPowerup)
+            {
+                // GameObject tmp = (GameObject)Instantiate(obstacle, new Vector3(0, 1, 42), Quaternion.identity);
+                GameObject tmp = (GameObject)GameObjectUtil.Instantiate(powerup, new Vector3(0, 1, 42));
+                //Vector3 pos = obstacle.transform.position;
+                tmp.transform.position = lanes[Random.Range(0, lanes.Length)].position;
+            } else
+            {
+                // GameObject tmp = (GameObject)Instantiate(obstacle, new Vector3(0, 1, 42), Quaternion.identity);
+                GameObject tmp = (GameObject)GameObjectUtil.Instantiate(obstacle, new Vector3(0, 1, 42));
+                //Vector3 pos = obstacle.transform.position;
+                tmp.transform.position = lanes[Random.Range(0, lanes.Length)].position;
+            }
             timeElapsed -= spawnCycle;
+            spawnPowerup = !spawnPowerup;
         }
     }
 }
