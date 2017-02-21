7b3898af7e5b081d13fd89365f0a66b74225f5f8
jasoncta <jasoncta@usc.edu>
2017-02-21 14:56:46 -0800

    Add live system
    Game will stop when lives is less than zero

--- Assets/Script/ScoreSystem.cs
+++ Assets/Script/ScoreSystem.cs
@@ -6,20 +6,33 @@ using UnityEngine.UI;
 public class ScoreSystem : MonoBehaviour {
 
 	public Text countText;
+	public Text countLives;
+	private const string LIVE_TEXT = "Lives: ";
+	private const string SCORE_TEXT = "Score: ";
 	private int count = 0;
+	private int lives = 3;
 
 	// Use this for initialization
 	void Start () {
-		SetCountText ();
+		SetText (countLives, LIVE_TEXT, lives.ToString ());
+		SetText(countText, SCORE_TEXT, count.ToString());
+
 	}
 
 	void OnTriggerEnter(Collider other){
 		Entity ent = other.GetComponent<Entity> ();
 		if (ent && ent.entityType == Entity.ENTITY_TYPE.ENEMY) {
 			//life decrease
+			if (lives - 1 == 0) {
+				Time.timeScale = 0;
+			}
+			lives--;
+			SetText (countLives, LIVE_TEXT, lives.ToString ());
+
 		} else {
 			count++;
-			SetCountText ();
+			//SetCountText ("Score:");
+			SetText(countText, SCORE_TEXT, count.ToString());
 		}
 
 	}
@@ -29,7 +42,9 @@ public class ScoreSystem : MonoBehaviour {
 		
 	}
 
-	void SetCountText() {
-		countText.text = "Score: " + count.ToString ();
+
+		
+	void SetText(Text text, string preMsg, string msg){
+		text.text = preMsg + msg;
 	}
 }
