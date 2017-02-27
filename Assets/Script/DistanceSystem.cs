b34d82c504625befb8ca4eb80721ded096e9f0ef
hanouri <hnouri@usc.edi>
2017-02-27 11:35:05 -0800

    score update_time

--- Assets/Script/DistanceSystem.cs
+++ Assets/Script/DistanceSystem.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class DistanceSystem : MonoBehaviour {
 	
 	public Transform player;
 
 	public Text countDistance;
+	public Text Totaltime;
 
 	public float distance;
+	public float t;
+	public float ellapsed;
+	public float rellapsed;
+
 
 	void Awake(){
 		distance = Vector3.Distance (player.position, transform.position);
-		//distance = 0;
+		distance = 0;
+		t = Time.time;
 	}
 
 	void Update(){
 		score ();
+		ellapsed = Time.time-t;
+		rellapsed = Mathf.Round (ellapsed);
+		Totaltime.text = "Timer: " + rellapsed.ToString ()+"  sec";
 	}
 	void score(){
 		distance += Vector3.Distance (player.position, transform.position);
