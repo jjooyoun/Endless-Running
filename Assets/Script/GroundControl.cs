75fc64e2c6bac0a9025e456f3418b5bf0465088a
hanouri <hnouri@usc.edi>
2017-02-28 22:51:47 -0800

    Added speed acceleration over time

--- Assets/Script/GroundControl.cs
+++ Assets/Script/GroundControl.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundControl : MonoBehaviour {
+	private DistanceSystem distancesystem;
+	float speed = 0.5f;
+	void Start()
+	{
+		distancesystem = FindObjectOfType<DistanceSystem> ();
 
+	}
     //Material texture offset rate
-    float speed = .5f;
 
     //Offset the material texture at a constant rate
     void Update()
     {
-        float offset = Time.time * speed;
+
+		if (distancesystem.rellapsed > 20) {
+			speed = 0.7f;
+		} else if (distancesystem.rellapsed > 60) {
+			speed = 0.9f;
+		}
+		  else if (distancesystem.rellapsed > 90) {
+			speed = 1f;
+		}
+			
+		float offset = Time.time * speed;
         GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, -offset);
-    }
+
+	}
 }
