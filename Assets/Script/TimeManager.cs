486ca9ea47b52fcbf4b25b1a8a81338bb1285c95
hoduc <duc_ho90@yahoo.com>
2017-02-28 17:26:57 -0800

    day_night take 2

--- Assets/Script/TimeManager.cs
+++ Assets/Script/TimeManager.cs
@@ -6,9 +6,11 @@ public class TimeManager : MonoBehaviour {
     public Transform moon;
     public float speed = 5.0f;
 	public Vector3 lookAt = Vector3.zero;
+
+	private ParticleSystem pSys;
 	// Use this for initialization
 	void Start () {
-	    
+		pSys = GetComponent<ParticleSystem> ();
 	}
 	
 
@@ -20,7 +22,22 @@ public class TimeManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if (sun.position.y < lookAt.y) {
+			Debug.Log ("star pulsing");
+			StarEnable (true);
+		} else {
+			StarEnable (false);
+		}
+
 		moveAround(sun, lookAt, Vector3.right, speed);
 		moveAround(moon, lookAt, Vector3.right, speed);
     }
+
+	void StarEnable(bool enabled){
+		if (enabled && !pSys.isPlaying) {
+			pSys.Play ();
+		} else if (!enabled && pSys.isPlaying) {
+			pSys.Stop ();
+		}
+	}
 }
