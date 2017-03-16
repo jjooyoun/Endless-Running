df3401062a44bcf90d67986b1232716239c28e18
hoduc <duc_ho90@yahoo.com>
2017-03-15 22:22:25 -0700

    Deformable part 1

---
+++ Assets/Script/SetRenderQueue.cs
@@ -0,0 +1,18 @@
+﻿/*
+	SetRenderQueue.cs
+ 
+	Sets the RenderQueue of an object's materials on Awake. This will instance
+	the materials, so the script won't interfere with other renderers that
+	reference the same materials.
+*/
+
+using UnityEngine;
+public class SetRenderQueue : MonoBehaviour {
+
+	public int renderQueue = 2002;
+	void Start(){
+		foreach (Renderer r in GetComponentsInChildren<Renderer>()) {
+			r.material.renderQueue = renderQueue;
+		}
+	}
+}
