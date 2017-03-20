e7e3a334db715df653c2a9eb8732c188074bc042
Jason Tan <jasoncta@usc.edu>
2017-03-19 17:20:52 -0700

    Add Intro Video

---
+++ Assets/Script/IntroScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroScript : MonoBehaviour {
+
+	public float time = 1; //Seconds to read the text
+	//public AudioSource audioSource;
+
+	public Text intro;
+	public Text title;
+	public GameObject summary;
+
+
+
+
+	// Use this for initialization
+	IEnumerator Start () {
+		
+		title.enabled = false;
+		summary.SetActive(false);
+		yield return new WaitForSeconds(3);
+		intro.enabled = false;
+		title.enabled = true;
+		yield return new WaitForSeconds(3);
+		title.enabled = false;
+		yield return new WaitForSeconds(time);
+		summary.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		summary.transform.Translate(0, 0.005f, 0);
+		
+	}
+}
