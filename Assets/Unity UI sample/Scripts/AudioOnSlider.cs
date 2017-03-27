2075a830472afeacb00c30418f17fab0d0fa6127
ImAKing <kla283@nyu.edu>
2017-03-27 14:35:40 -0700

    menu

---
+++ Assets/Unity UI sample/Scripts/AudioOnSlider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioOnSlider : MonoBehaviour {
+	public Slider slider;
+	public AudioSource music;
+	private float prevVolume;
+	// Use this for initialization
+	void Start () {
+		prevVolume = music.volume;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("volume:" + music.volume);
+	}
+
+	public void OnVaueChanged(){
+		Debug.Log ("value changed to whatever" + slider.value);
+
+		music.volume = slider.value;
+		//prevVolume = music.volume;
+	}
+}
