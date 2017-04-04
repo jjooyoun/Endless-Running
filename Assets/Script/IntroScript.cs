60496282737d39ce15e4cae85a3fceef56055f5e
hoduc <duc_ho90@yahoo.com>
2017-04-04 15:15:28 -0700

    Intro Scene multiple screen size

--- Assets/Script/IntroScript.cs
+++ Assets/Script/IntroScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class IntroScript : MonoBehaviour {
 
@@ -11,13 +12,13 @@ public class IntroScript : MonoBehaviour {
 	public Text intro;
 	public Text title;
 	public GameObject summary;
-
-
-
-
+	private Bounds summaryTextBound;
+	private float screenY;
+	private float extentY;
+	public float speedUp = 0.3f;
 	// Use this for initialization
 	IEnumerator Start () {
-		
+		screenY = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0.0f)).y;
 		title.enabled = false;
 		//summary.SetActive(false);
 		yield return new WaitForSeconds(3);
@@ -31,8 +32,11 @@ public class IntroScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		summary.transform.Translate(0, 0.3f, 0);
-		
+		summary.transform.Translate(0, speedUp, 0);
+	}
+
+
+	void DoActivateTrigger(){
+		SceneManager.LoadScene (0);
 	}
 }
