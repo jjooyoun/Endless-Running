4767f51a202fae2262782c3960578d0a09ae5104
hanouri <hnouri@usc.edi>
2017-04-10 14:38:18 -0700

    level score summary

---
+++ Assets/Script/level1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class level1: MonoBehaviour {
+	private static int playerScore;  //  A new Static variable to hold our score.
+	public Text scoreText;
+
+	void Start()
+	{
+		playerScore = ScoreSystem.count;  //  Update our score
+		scoreText.text = "Your score: " + playerScore.ToString ();
+	}
+}
