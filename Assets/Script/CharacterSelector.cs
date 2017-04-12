3afaf82b4466e7dd65b0e78b303a7b725f64255c
Jooyoun Lee <jjooyoun@gmail.com>
2017-04-12 01:15:31 -0700

    CharacterSelector Proto Start

---
+++ Assets/Script/CharacterSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour {
+
+    public List<GameObject> characterList;
+
+    void Start () {
+        GameObject character = Resources.Load("Prefabs/Snowball") as GameObject;
+        GameObject _char = Instantiate(character) as GameObject;
+        _char.transform.SetParent(GameObject.Find("CharacterList").transform);
+        characterList.Add(_char);
+        _char.SetActive(false);
+        characterList[0].SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
