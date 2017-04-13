d2e955a1e150fc7149afaa279a60fbcd5750ed1f
Jooyoun Lee <jjooyoun@gmail.com>
2017-04-12 22:02:57 -0700

    CharSelector More

--- Assets/Script/CharacterSelector.cs
+++ Assets/Script/CharacterSelector.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class CharacterSelector : MonoBehaviour {
 
     public List<GameObject> characterList;
+    public int index = 0;
 
     void Start () {
-        GameObject character = Resources.Load("Prefabs/Snowball") as GameObject;
-        GameObject _char = Instantiate(character) as GameObject;
-        _char.transform.SetParent(GameObject.Find("CharacterList").transform);
-        characterList.Add(_char);
-        _char.SetActive(false);
-        characterList[0].SetActive(true);
-	}
+        GameObject[] character = Resources.LoadAll<GameObject>("Prefabs/Character");
+        foreach (GameObject c in character) {
+            GameObject _char = Instantiate(c) as GameObject;
+            _char.transform.SetParent(GameObject.Find("CharacterList").transform);
+            characterList.Add(_char);
+            _char.SetActive(false);
+            characterList[index].SetActive(true);
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void Next() {
+        characterList[index].SetActive(false);
+        if (index == characterList.Count - 1) {
+            index = 0;
+        } else {
+            index++;
+        }
+        characterList[index].SetActive(true);
+    }
+
+    public void Previous() {
+        characterList[index].SetActive(false);
+        if (index == 0) {
+            index = characterList.Count - 1;
+        } else {
+            index--;
+        }
+        characterList[index].SetActive(true);
+    }
 }
