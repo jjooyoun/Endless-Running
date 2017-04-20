1321ea14a492847cc096ab0c0cebb2ae9f84dd83
jasoncta <jasoncta@usc.edu>
2017-04-20 13:03:08 -0700

    Fix TIE enemy laser beam sound

--- Assets/Script/LaserBeam.cs
+++ Assets/Script/LaserBeam.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserBeam : MonoBehaviour {
+
+	public AudioClip shootSound;
+	private AudioSource source;
+	bool isFired = false;
 	/// <summary>
 	/// OnParticleCollision is called when a particle hits a collider.
 	/// </summary>
 	/// <param name="other">The GameObject hit by the particle.</param>
+	void Awake() {
+		source = GetComponent<AudioSource>();
+	}
+
+	void Start() {
+		StartCoroutine(PlaySoundLaser());
+	}
+
 	void OnParticleCollision(GameObject other)
 	{
 		//Debug.Log("ghello:"+other.name);
@@ -22,5 +34,23 @@ public class LaserBeam : MonoBehaviour {
 
 			}
 		}
+	}
+
+	IEnumerator PlaySoundLaser()
+	{
+		yield return new WaitForSeconds(2);
+		Debug.Log ("TIE Fire sound");
+		//source.clip = shootSound;
+		source.PlayOneShot(shootSound,1);
+	}
+
+	void Update() {
+		//Debug.Log ("isFire:" + isFired);
+		//Debug.Log ("Time.time:" + Time.time);
+		if (isFired == false) {
+			//PlaySoundLaser ();
+			isFired = true;
+		}
+
 	}
 }
